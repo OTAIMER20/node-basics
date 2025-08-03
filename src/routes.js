cfc7873ae36f3996221f33bf7fe1826f8dcaaf36
OTAIMER20 <otaimer@outlook.com>
2025-08-03 22:48:50 +0200

    Add Regex

--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,21 @@
+import path from 'path';
 import { Database } from './database.js';
 import { randomUUID } from 'crypto';
+import { buildRoutePath } from './utils/build-route-path.js';
 
 const database = new Database()
 
 export const routes = [
     {
         method: 'GET',
-        path: '/users',
+        path: buildRoutePath('/users'),
         handler: (req, res) => {
-            const users = database.select('users')
+            const { search } = req.query
+
+            const users = database.select('users', {
+                name: search,
+                email: search
+            })
             
             return res.end(JSON.stringify(users));
         }
@@ -16,7 +23,7 @@ export const routes = [
 
     {
         method: 'POST',
-        path: '/users',
+        path: buildRoutePath('/users'),
         handler: (req, res) => {
             const { name, email } = req.body
 
@@ -34,6 +41,30 @@ export const routes = [
     },
 
     {
+        method: 'PUT',
+        path: buildRoutePath('/users/:id'),
+        handler: (req, res) => {
+            const { id } = req.params
+            const { name, email } = req.body
 
+            database.update('users', id, {
+                name,
+                email
+            })
+
+            return res.writeHead(204).end()
+        },
+    },
+
+        {
+        method: 'DELETE',
+        path: buildRoutePath('/users/:id'),
+        handler: (req, res) => {
+            const { id } = req.params
+
+            database.delete('users', id)
+
+            return res.writeHead(204).end()
+        },
     }
 ]
